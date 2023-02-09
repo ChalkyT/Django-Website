43a9fe971d0825f4366c9430af8ddbde9d0d9018
ChalkyT <rhian.cremer@madetech.com>
2023-02-09 12:09:33 +0000

    change staticfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n8yx*&p*kob*_c^u+u=$hxcthtl^6tce41@enp5hi#1-h%qdlf" # this is to replace the secret key you cut away before
+SECRET_KEY = "django-insecure-n8yx*&p*kob*_c^u+u=$hxcthtl^6tce41@enp5hi#1-h%qdlf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
